6b8c0ade2f4c7e047ad17ff6922d8297d834c705
Mewan <damithamewan201@gmail.com>
2024-05-22 13:44:15 +0530

    Build fixes on dynamic metadata

--- src/app/(app)/interview-questions/[id]/layout.tsx
+++ src/app/(app)/interview-questions/[id]/layout.tsx
@@ -1,17 +1,18 @@
 import { Metadata } from 'next';
-import { SearchParamProps } from '@/types';
 import { getTitleCase } from '@/utils';
 
 export async function generateMetadata({
 	params,
-}: SearchParamProps): Promise<Metadata> {
+}: {
+	params: { id: string };
+}): Promise<Metadata> {
 	return {
 		title: `${getTitleCase(params.id)} Interview Questions`,
 		// TODO: Add a description
 	};
 }
 
-export default function Layout({
+export default async function Layout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
